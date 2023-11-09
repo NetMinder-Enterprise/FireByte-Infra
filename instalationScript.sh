0752a1c8c19a78fcd0712138b9cdb144fdd4fc4f
Danilo <danilo.pedrazzi@sptech.school>
2023-11-09 20:59:39 -0300

    Update URLs + Verify docker instalation + Add gitignore

--- instalationScript.sh
+++ instalationScript.sh
@@ -13,31 +13,44 @@ mkdir DB
 mkdir Java
 
 #Create executable for running the app
-#wget Firebyte.sh
+wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Infra/main/Firebyte.sh"
+
+#Setup Docker
+echo "\033[0;34mVerificando se o docker está instalado..."
+docker --version
+if [ $? = 0 ];
+  then
+    echo "\033[0;34mInstalando o docker..."
+    sudo apt install docker.io
+    echo "\033[0;34mDocker instalado com sucesso!"
+fi
+echo "\033[0;34mIniciando o docker..."
+sudo systemctl start docker
+sudo systemctl enable docker
+echo "\033[0;34mDocker iniciado!"
 
 #MySQL Container
 echo "\033[0;34mConfigurando Banco Local..."
 cd DB
 mkdir sql
 cd sql
-wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Backend/main/src/main/java/entities/createUser.sql"
-wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Backend/main/src/main/java/entities/Script%20DB.sql"
+wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Infra/main/DB/createUser.sql"
+wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Infra/main/DB/Script%20DB.sql"
 cd ..
-#wget Dockerfile DB
+wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Infra/main/DB/Dockerfile"
 docker build -t firebytedb .
 docker run -d --name localDB -p 3306:3306 firebytedb
 cd ..
 echo "\033[0;34mBanco Local configurado com sucesso!"
 
-#Java Container
+#Java .jar
 cd Java
-echo "\033[0;34mConfigurando Sistema Java..."
+echo "\033[0;34mBaixando Sistema Java..."
 #wget .jar
-echo "\033[0;34mDigite seu usuário para acessar o serviço Firebyte:"
-read usuario
-echo "\033[0;34mDigite sua senha:"
-read senha
-#wget Dockerfile Java
+
+#Java Container
+echo "\033[0;34mConfigurando Sistema Java..."
+wget "https://raw.githubusercontent.com/NetMinder-Enterprise/FireByte-Infra/main/Java/Dockerfile"
 docker build -t firebyteJava .
 docker run -p 8080:8080 firebyteJava
 echo "\033[0;34mFirebyte foi configurado com sucesso!"
