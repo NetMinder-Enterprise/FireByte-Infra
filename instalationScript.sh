c259034188a81be6b2acc32b76b4dd2fd1598d4c
Danilo <danilo.justino1@gmail.com>
2023-11-10 22:31:01 -0300

    Fix Docker Container Verifier + Fix Print Logic

--- instalationScript.sh
+++ instalationScript.sh
@@ -72,27 +72,27 @@ start_docker(){
 
 # Builda e inicia o container do DB
 build_db_container(){
-  if docker ps -a -q -f name=firebytedb
+  if [ ! "$(docker ps -a -q -f name=<name>)" ];
     then
-      printf "${GREEN}Container DB já está rodando!${NC} \n"
-    else
       cd DB || exit_with_error "Diretório não encontrado: DB."
       docker build -t firebytedb . || exit_with_error "Falha ao construir a imagem Docker para o banco de dados."
       docker run -d --name firebytedb -p 3306:3306 firebytedb || exit_with_error "Falha ao iniciar o contêiner MySQL."
       cd .. || exit_with_error "Falha ao voltar para o diretório FireByte."
+    else
+      printf "${GREEN}Container DB já está rodando!${NC} \n"
   fi
 }
 
 # Builda e inicia o container do Java
 build_java_container(){
-  if docker ps -a -q -f name=firebytejava
+  if [ ! "$(docker ps -a -q -f name=<name>)" ];
     then
-      printf "${GREEN}Container Java já está rodando!${NC} \n"
-    else
       cd Java || exit_with_error "Diretório não encontrado: java."
       docker build -t firebytejava . || exit_with_error "Falha ao construir a imagem Docker para o aplicativo Java."
       docker run -d --name firebytejava -p 8080:8080 firebytejava || exit_with_error "Falha ao iniciar o contêiner Java."
       cd .. || exit_with_error "Falha ao voltar para o diretório FireByte."
+    else
+      printf "${GREEN}Container Java já está rodando!${NC} \n"
   fi
 }
 
@@ -123,7 +123,8 @@ build_db_container
 printf "${GREEN}Container do Banco Local iniciado com sucesso!${NC} \n"
 
 printf "${PURPLE}Iniciando container Java...${NC} \n"
+build_java_container
+printf "${GREEN}Container Java iniciado com sucesso!${NC} \n"
 
 printf "${GREEN}Firebyte foi configurado com sucesso!${NC} \n"
-build_java_container
 printf "${PURPLE}Para executar nosso programa novamente, você pode rodar o script 'Firebyte.sh'${NC} \n"
